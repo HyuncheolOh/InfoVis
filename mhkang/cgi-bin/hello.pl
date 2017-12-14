feb4017c10017fd9ba93c42cfd79564ac16d2a91
KangMinHyeok <saigoyo@nate.com>
2017-12-15 00:20:34 +0900

    mhkang added
    제 작업공간 추가한 버전입니다.

---
+++ mhkang/cgi-bin/hello.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+print <<END_of_HTML;
+Content-type: text/html
+
+<html>
+	<head>
+  		<title> Hello</title>
+ 	</head>
+	<body>
+ 	<h1> Hello World</h1>
+  	<p>Hello everybody.</p>
+ 	</body>
+</html>
+END_of_HTML
